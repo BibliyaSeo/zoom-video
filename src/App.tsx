caa2dacfb77a6ae598a5ed6ca8d0ecab0bc2ec58
BibilyaSeo <seongkyeong.seo@gmail.com>
2023-01-02 11:56:13 +0900

    feat: firebase Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { EuiProvider } from "@elastic/eui";
+import { EuiProvider, EuiThemeProvider } from "@elastic/eui";
 import React from "react";
 import "@elastic/eui/dist/eui_theme_light.css";
 import { Route, Routes } from "react-router-dom";
@@ -6,13 +6,22 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const overrides = {
+    colors: {
+      LIGHT: { primary: "#0b5cff" },
+      DARK: { primary: "#0b5cff" },
+    },
+  };
+
   return (
     <EuiProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<Dashboard />} />
-      </Routes>
+      <EuiThemeProvider modify={overrides}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
+        </Routes>
+      </EuiThemeProvider>
     </EuiProvider>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,71 @@
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiImage,
+  EuiPanel,
+  EuiProvider,
+  EuiSpacer,
+  EuiText,
+  EuiTextColor,
+} from "@elastic/eui";
 import React from "react";
+import animation from "../assets/animation.gif";
+import logo from "../assets/logo.png";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseAuth, userRef } from "../utils/FirebaseConfig";
+import { query, where, addDoc, getDocs } from "@firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const login = async () => {
+    const provider = new GoogleAuthProvider();
+    const {
+      user: { displayName, email, uid },
+    } = await signInWithPopup(firebaseAuth, provider);
+    if (email) {
+      const firestoreQuery = query(userRef, where("uid", "==", uid));
+      const fetchedUsers = await getDocs(firestoreQuery);
+      if (fetchedUsers.docs.length === 0) {
+        await addDoc(userRef, { uid, name: displayName, email });
+      }
+    }
+    navigate("/");
+  };
+
+  return (
+    <EuiProvider colorMode="dark">
+      <EuiFlexGroup
+        alignItems="center"
+        justifyContent="center"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <EuiFlexItem grow={false}>
+          <EuiPanel paddingSize="xl">
+            <EuiFlexGroup justifyContent="center" alignItems="center">
+              <EuiFlexItem>
+                <EuiImage src={animation} alt="animation" />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiImage src={logo} alt="logo" size={"230px"} />
+                <EuiSpacer size="xs" />
+                <EuiText textAlign="center" grow={false}>
+                  <h3>
+                    <EuiTextColor>One Platform to</EuiTextColor>
+                    <EuiTextColor color={"#0b5cff"}> connect</EuiTextColor>
+                  </h3>
+                </EuiText>
+                <EuiSpacer size="l" />
+                <EuiButton fill onClick={login}>
+                  Login with Google
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiProvider>
+  );
 }
